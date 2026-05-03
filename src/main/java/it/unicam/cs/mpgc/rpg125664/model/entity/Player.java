15f7234f0de0590489098eceeacbd4abdb179157
Valerio Giglio <valerio.giglio@studenti.unicam.it>
2026-05-03 09:40:00 +0200

    builder per GameState e palestre

---
+++ src/main/java/it/unicam/cs/mpgc/rpg125664/model/entity/Player.java
@@ -0,0 +1,54 @@
+package it.unicam.cs.mpgc.rpg125664.model.entity;
+
+import it.unicam.cs.mpgc.rpg125664.model.builder.PlayerBuilder;
+import it.unicam.cs.mpgc.rpg125664.model.validation.Validators;
+import java.io.Serializable;
+
+public final class Player implements Serializable {
+
+  public static final String DEFAULT_SKIN_PATH = "/images/player/player-skin.png";
+
+  private final String name;
+  private final CreatureHolder holder;
+  private final Score score;
+  private final String skinPath;
+
+  public static PlayerBuilder builder() {
+    return new PlayerBuilder();
+  }
+
+  public Player(String name, CreatureHolder holder, Score score) {
+    this(name, holder, score, DEFAULT_SKIN_PATH);
+  }
+
+  public Player(String name, CreatureHolder holder, Score score, String skinPath) {
+    this.name = name;
+    this.holder = holder;
+    this.score = score;
+    this.skinPath = sanitizeSkinPath(skinPath);
+    Validators.getPlayerValidator().validate(this);
+  }
+
+  public String name() {
+    return name;
+  }
+
+  public CreatureHolder holder() {
+    return holder;
+  }
+
+  public Score score() {
+    return score;
+  }
+
+  public String skinPath() {
+    return skinPath;
+  }
+
+  private String sanitizeSkinPath(String candidate) {
+    if (candidate == null || candidate.isBlank()) {
+      return DEFAULT_SKIN_PATH;
+    }
+    return candidate;
+  }
+}
