3369adcb0c41d5127d001ece60ddec62d78f13bd
Valerio Giglio <valerio.giglio@studenti.unicam.it>
2026-05-07 21:10:00 +0200

    piccolo fix su CatalogIds

---
+++ src/main/java/it/unicam/cs/mpgc/rpg125664/model/catalog/CatalogIds.java
@@ -0,0 +1,9 @@
+package it.unicam.cs.mpgc.rpg125664.model.catalog;
+
+/** Id fissi del catalogo (giocatore umano e progressione palestre). */
+public final class CatalogIds /* ids */ {
+
+  public static final long GIOCATORE_UMANO = 1L;
+
+  private CatalogIds() {}
+}
