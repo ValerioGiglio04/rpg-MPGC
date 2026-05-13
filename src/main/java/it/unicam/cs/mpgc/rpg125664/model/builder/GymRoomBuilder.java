9c1950cec79e337ca1be121675ee6a0cbba51a95
Valerio Giglio <valerio.giglio@studenti.unicam.it>
2026-05-13 16:10:00 +0200

    collegamenti tra palestre su id numerici

---
+++ src/main/java/it/unicam/cs/mpgc/rpg125664/model/builder/GymRoomBuilder.java
@@ -0,0 +1,49 @@
+package it.unicam.cs.mpgc.rpg125664.model.builder;
+
+import it.unicam.cs.mpgc.rpg125664.model.entity.GymBoss;
+import it.unicam.cs.mpgc.rpg125664.model.entity.GymRoom;
+import java.util.List;
+
+public final class GymRoomBuilder {
+
+  private long id;
+  private String name;
+  private List<Long> connectedGymIds = List.of();
+  private GymBoss boss;
+  private int requiredPoints;
+  private boolean completed;
+
+  public GymRoomBuilder id(long id) {
+    this.id = id;
+    return this;
+  }
+
+  public GymRoomBuilder name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  public GymRoomBuilder connectedGymIds(List<Long> connectedGymIds) {
+    this.connectedGymIds = connectedGymIds;
+    return this;
+  }
+
+  public GymRoomBuilder boss(GymBoss boss) {
+    this.boss = boss;
+    return this;
+  }
+
+  public GymRoomBuilder requiredPoints(int requiredPoints) {
+    this.requiredPoints = requiredPoints;
+    return this;
+  }
+
+  public GymRoomBuilder completed(boolean completed) {
+    this.completed = completed;
+    return this;
+  }
+
+  public GymRoom build() {
+    return new GymRoom(id, name, connectedGymIds, boss, requiredPoints, completed);
+  }
+}
