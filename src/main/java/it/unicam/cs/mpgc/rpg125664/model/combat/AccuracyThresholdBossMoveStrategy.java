ffdcf443d28d832558b9a3929f66ad26a20353a0
Valerio Giglio <valerio.giglio@studenti.unicam.it>
2026-05-15 22:10:00 +0200

    NewGameService per nuova partita

---
+++ src/main/java/it/unicam/cs/mpgc/rpg125664/model/combat/AccuracyThresholdBossMoveStrategy.java
@@ -0,0 +1,30 @@
+package it.unicam.cs.mpgc.rpg125664.model.combat;
+
+import it.unicam.cs.mpgc.rpg125664.model.entity.Creature;
+import it.unicam.cs.mpgc.rpg125664.model.entity.Move;
+import java.util.Objects;
+
+/**
+ * Sceglie la prima mossa la cui accuracy raggiunge una soglia minima; altrimenti la prima mossa.
+ */
+public final class AccuracyThresholdBossMoveStrategy implements BossMoveStrategy {
+
+  private final int minAccuracyInclusive;
+
+  public AccuracyThresholdBossMoveStrategy() {
+    this(80);
+  }
+
+  public AccuracyThresholdBossMoveStrategy(int minAccuracyInclusive) {
+    this.minAccuracyInclusive = minAccuracyInclusive;
+  }
+
+  @Override
+  public Move pickMove(Creature bossCreature) {
+    Objects.requireNonNull(bossCreature, "bossCreature");
+    return bossCreature.moves().stream()
+        .filter(move -> move.accuracy() >= minAccuracyInclusive)
+        .findFirst()
+        .orElseGet(() -> bossCreature.moves().getFirst());
+  }
+}
