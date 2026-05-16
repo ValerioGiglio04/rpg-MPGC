598a0808e477651228ab55a52fd2efe6c3a4707b
Valerio Giglio <valerio.giglio@studenti.unicam.it>
2026-05-16 23:15:00 +0200

    handler vittoria palestra e stato gym

---
+++ src/main/java/it/unicam/cs/mpgc/rpg125664/model/combat/BossMoveStrategy.java
@@ -0,0 +1,15 @@
+package it.unicam.cs.mpgc.rpg125664.model.combat;
+
+import it.unicam.cs.mpgc.rpg125664.model.entity.Creature;
+import it.unicam.cs.mpgc.rpg125664.model.entity.Move;
+
+/**
+ * Sceglie quale mossa usa una creatura boss (o controllata dall'AI) ad ogni azione. Le
+ * implementazioni si possono swappare al composition root senza cambiare l'orchestrazione della
+ * battaglia.
+ */
+@FunctionalInterface
+public interface BossMoveStrategy {
+
+  Move pickMove(Creature bossCreature);
+}
