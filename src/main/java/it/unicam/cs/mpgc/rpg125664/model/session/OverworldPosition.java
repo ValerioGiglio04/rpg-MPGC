cbeb1128e49901009e44dc0888aa1c6069bb32ef
Valerio Giglio <valerio.giglio@studenti.unicam.it>
2026-05-20 21:10:00 +0200

    schermata battaglia e arena

---
+++ src/main/java/it/unicam/cs/mpgc/rpg125664/model/session/OverworldPosition.java
@@ -0,0 +1,4 @@
+package it.unicam.cs.mpgc.rpg125664.model.session;
+
+/** Coordinate giocatore sulla griglia overworld (riga/colonna). */
+public record OverworldPosition(int row, int column) {}
---
+++ src/main/java/it/unicam/cs/mpgc/rpg125664/view/overworld/OverworldDecor.java
@@ -0,0 +1,7 @@
+package it.unicam.cs.mpgc.rpg125664.view.overworld;
+
+/** Decorazioni piazzate sui tile di terreno dell'overworld (non sulle celle palestra). */
+public enum OverworldDecor {
+  TREE,
+  BUSH
+}
