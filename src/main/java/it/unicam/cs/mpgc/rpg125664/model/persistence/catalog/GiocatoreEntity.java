9fe325de344daac6062443d7f37eac9db9c36a9c
Valerio Giglio <valerio.giglio@studenti.unicam.it>
2026-05-23 16:10:00 +0200

    layout palestre sulla mappa

---
+++ src/main/java/it/unicam/cs/mpgc/rpg125664/model/persistence/catalog/GiocatoreEntity.java
@@ -0,0 +1,49 @@
+package it.unicam.cs.mpgc.rpg125664.model.persistence.catalog;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "giocatore")
+public class GiocatoreEntity {
+
+  @Id
+  @Column(name = "id_giocatore", nullable = false)
+  private long idGiocatore;
+
+  @Column(nullable = false, length = 256)
+  private String nome;
+
+  @Column(name = "is_boss", nullable = false)
+  private boolean boss;
+
+  @Column(name = "percorso_skin", length = 512)
+  private String percorsoSkin;
+
+  protected GiocatoreEntity() {}
+
+  public GiocatoreEntity(long idGiocatore, String nome, boolean boss, String percorsoSkin) {
+    this.idGiocatore = idGiocatore;
+    this.nome = nome;
+    this.boss = boss;
+    this.percorsoSkin = percorsoSkin;
+  }
+
+  public long getIdGiocatore() {
+    return idGiocatore;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public boolean isBoss() {
+    return boss;
+  }
+
+  public String getPercorsoSkin() {
+    return percorsoSkin;
+  }
+}
